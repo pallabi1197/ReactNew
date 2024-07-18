f94807fcb31784e6fd995876b3c579ec4d5176c1
Pallabi Das <pallabi.das@sgate.net>
2024-07-18 12:20:04 +0530

    Updated

--- src/Usereduser.jsx
+++ src/Usereduser.jsx
@@ -1,17 +1,35 @@
 import React, { useReducer, useState } from "react";
 
-// useReducer is same as useState, the difference is manage multiple states on a big project we use useReducer hook.
-// reducer are pure function to accept state and actions.
+// useReducer is same as useState, the difference is manage multiple states on a big project.
+// reducer function are pure function to accept state and actions.
+//Pure function - The function always returns the same output if the same arguments passed in.
+// The function does not produce any side-effects.
+// Role of dispatch to trigger the action method.
+
+
+const intialState = 0;
+
+const reducer = (state, action) => {
+  if (action.type === "INCREMENT") {
+    return state + 1;
+  }
+
+  if (action.type === "DECREMENT") {
+    return state - 1;
+  }
+
+
+  return state;
+};
 
 const Usereduser = () => {
-  const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, intialState);
 
   return (
     <div className="btnstyle">
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <p>{count}</p>
-      <button onClick={() =>  setCount(count - 1)}>Decrement</button>
-
+      <button onClick={() => dispatch({ type: "INCREMENT" })}>Inc</button>
+      <p>{state}</p>
+      <button onClick={() => dispatch({ type: "DECREMENT" })}>Dec</button>
     </div>
   );
 };
