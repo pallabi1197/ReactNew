066c0a7ea8b25bea8d9ddb69877d7481b1606e03
Pallabi Das <pallabi.das@sgate.net>
2024-07-24 18:59:30 +0530

    ok

--- src/Globalstyle.jsx
+++ src/Globalstyle.jsx
@@ -99,5 +99,69 @@ li {
    grid-template-columns: 1fr 1.2fr .5fr .8fr ;
 }
 
+.common-heading {
+  font-size: 3.8rem;
+  font-weight: 600;
+  margin-bottom: 6rem;
+  text-transform: capitalize;
+}
+
+input, textarea{
+max-width: 50rem;
+color: ${({ theme }) => theme.colors.black};
+padding: 1.6rem 2.4rem;
+border: 1px solid ${({ theme }) => theme.colors.border};
+text-transform: uppercase;
+box-shadow: ${({ theme }) => theme.colors.shadowSupport};
+}
+input[type="submit"]{
+max-width: 16rem;
+margin-top: 2rem;
+background-color: ${({ theme }) => theme.colors.btn};
+color: ${({ theme }) => theme.colors.white};
+padding: 1.4rem 2.2rem;
+border-style: solid;
+border-width: .1rem;
+text-transform: uppercase;
+font-size: 1.8rem;
+cursor: pointer;
+}
+
+
+/* ===========================================
+/* media queries  
+======================================= */
+/* px  */
+/* rem  */
+/* em  */
+/* 1500px */
+
+//998px
+@media (max-width:${({ theme }) => theme.media.tab}) {
+  .container{
+    padding: 0 3.2rem;
+  }
+
+    .grid-three-column {
+  grid-template-columns: 1fr 1fr;
+}
+}
+
+@media (max-width:${({ theme }) => theme.media.mobile}) {
+
+  html{
+    font-size: 50%;
+  }
+
+  .grid{
+    gap: 3.2rem;
+  }
+
+  .grid-two-column, .grid-three-column, .grid-four-column{
+    grid-template-columns: 1fr;
+  }
+}
+
+
 
 `;
